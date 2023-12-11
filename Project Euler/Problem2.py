151bafe73eee1b2435cacd2b5dbd7d6de7c3a819
gherkins05 <153504167+gherkins05@users.noreply.github.com>
2023-12-11 13:18:04 +0000

    Added Code

---
+++ Project Euler/Problem2.py
@@ -0,0 +1,11 @@
+"""By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."""
+sum = 0
+fibValues = [1, 1]
+while fibValues[-1] < 4000000:
+    fibValues.append(fibValues[-1] + fibValues[-2])
+
+for value in fibValue:
+    if value % 2 == 0:
+        sum  += value
+
+print(sum)
